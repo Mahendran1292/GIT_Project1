427c5b8fc0c34d33583f265b651e9a2c32a3555e
Mahendran1292 <mahendranc1292@gmail.com>
2024-08-15 14:44:53 +0530

    Demo Project Repository

---
+++ src/org/gitdemoproject1/GitDemoProject1.java
@@ -0,0 +1,13 @@
+package org.gitdemoproject1;
+
+public class GitDemoProject1 {
+	private void today() {
+		System.out.println("Independence Day");
+
+	}
+	public static void main(String[] args) {
+		GitDemoProject1 git = new GitDemoProject1();
+		git.today();
+	}
+
+}
